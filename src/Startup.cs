6c9c811036e67b56d8e66e7cf8cd677d371cc82c
Tobias Moe Thorstensen <tobias@enso.no>
2021-03-27 12:42:49 +0100

    changed startup to read from http headers

--- src/Startup.cs
+++ src/Startup.cs
@@ -8,11 +8,20 @@ namespace CloudCollective
     {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            const string HEADER_NAME = "Azure-Frontdoor-Header";
+
             app.Use(async (ctx, next) =>
             {
                 if(ctx.Request.Method.ToLower() == "get")
                 {
-                    await ctx.Response.WriteAsync("Hello, world!");
+                    if(ctx.Request.Headers.ContainsKey(HEADER_NAME))
+                    {
+                        var header = ctx.Request.Headers[HEADER_NAME];
+                        await ctx.Response.WriteAsync($"Found Azure Frontdoor Header with value: '{header}'");
+                        return;
+                    }
+
+                    await ctx.Response.WriteAsync($"Unable to find HTTP Header: '{HEADER_NAME}'");
                 }
             });
         }
