f10895f7b4c30944235b31d0e8251f709e27c516
Tobias Moe Thorstensen <tobias@enso.no>
2021-03-27 13:45:29 +0100

    reading environment variables

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,20 +9,12 @@ namespace CloudCollective
     {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            const string HEADER_NAME = "Azure-Frontdoor-Header";
-
             app.Use(async (ctx, next) =>
             {
                 if(ctx.Request.Method.ToLower() == "get")
                 {
-                    if(ctx.Request.Headers.ContainsKey(HEADER_NAME))
-                    {
-                        var header = ctx.Request.Headers[HEADER_NAME];
-                        await ctx.Response.WriteAsync($"Found Azure Frontdoor Header with value: '{header}'");
-                        return;
-                    }
-
-                    await ctx.Response.WriteAsync($"Unable to find HTTP Header: '{HEADER_NAME}'");
+                    var appName = Environment.GetEnvironmentVariable("AppName");
+                    await ctx.Response.WriteAsync($"Greetings from '{appName}'");
                 }
             });
         }
